dce288566e3caf354c76f0981ecfeb707c66727d
hieudinh <dinhvanhieu2002@gmail.com>
2023-11-03 20:02:42 +0700

    update route post

--- src/routes/post.js
+++ src/routes/post.js
@@ -5,7 +5,7 @@ const auth = require('../middlewares/auth')
 //done
 router.post("/", auth, postController.create);
 router.get("/", auth, postController.getPosts);
-router.get("/:userId", auth, postController.getPostsOfUser);
+router.get("/user/:userId", auth, postController.getPostsOfUser);
 router.get("/:postId", auth, postController.getPostById);
 router.post("/:postId/reaction", auth, postController.reaction);
 router.delete("/:postId", auth, postController.remove);
