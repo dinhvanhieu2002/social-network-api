03a550ea4ddfc5fa0437880108eb081b7689ec29
hieudinh <dinhvanhieu2002@gmail.com>
2023-12-11 22:32:51 +0700

    update socket io

--- src/controllers/ChatController.js
+++ src/controllers/ChatController.js
@@ -1,20 +1,20 @@
 const Message = require("../models/Message");
 
 module.exports = (io) => {
-  let users = [];
+  // let users = [];
 
-  const addUser = (userId, socketId) => {
-    !users.some((user) => user.userId === userId) &&
-      users.push({ userId, socketId });
-  };
+  // const addUser = (userId, socketId) => {
+  //   !users.some((user) => user.userId === userId) &&
+  //     users.push({ userId, socketId });
+  // };
 
-  const removeUser = (socketId) => {
-    users = users.filter((user) => user.socketId !== socketId);
-  };
+  // const removeUser = (socketId) => {
+  //   users = users.filter((user) => user.socketId !== socketId);
+  // };
 
-  const getUser = (userId) => {
-    return users.find((user) => user.userId === userId);
-  };
+  // const getUser = (userId) => {
+  //   return users.find((user) => user.userId === userId);
+  // };
 
   io.on("connection", (client) => {
     console.log("new connection");
@@ -22,19 +22,25 @@ module.exports = (io) => {
     client.on("disconnect", () => {
       client.broadcast.emit("user disconnected");
       console.log("user disconnected");
-      removeUser(client.id);
-      io.emit("getUsers", users);
+      // removeUser(client.id);
+      // io.emit("getUsers", users);
     });
 
-    client.on("addUser", (userId) => {
-      addUser(userId, client.id);
-      io.emit("getUsers", users);
+    // client.on("addUser", (userId) => {
+    //   addUser(userId, client.id);
+    //   io.emit("getUsers", users);
+    // });
+
+    client.on("join", (conversationId) => {
+      client.join(conversationId);
     });
 
     client.on("sendMessage", (data) => {
-      const jsonData = JSON.parse(data);
-      const user = getUser(jsonData['receivedId']);
-      io.to(user?.socketId).emit("getMessage", data);
+      // const jsonData = JSON.parse(data);
+      // const user = getUser(jsonData['receivedId']);
+      // io.to(user?.socketId).emit("getMessage", data);
+      client.broadcast.to(data.conversationId).emit("getMessage", data);
+
     });
     
   });
